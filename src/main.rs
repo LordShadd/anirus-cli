304aa430cdf794c4dc8899d4f5dbf08b47772374
LordShadd <real.davidwilson@proton.me>
2023-09-12 08:43:42 -0300

    feat: platform types

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::Result;
 
+mod types;
+
 #[tokio::main]
 async fn main() -> Result<()> {
     Ok(())
---
+++ src/types.rs
@@ -0,0 +1,5 @@
+pub enum SourcePlatform {
+    AnimeFire,
+    AnimesZone,
+    AnRoll,
+}

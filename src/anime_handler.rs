3aa28c384e711d94c424d570fd2f678c4e59d6e6
LordShadd <real.davidwilson@proton.me>
2023-09-17 09:31:56 -0300

    feat: initial anime handler

--- src/anime_handler.rs
+++ src/anime_handler.rs
@@ -1,13 +1,27 @@
-use crate::{fetch::Fetchable, scrape::Scrapable};
+use anyhow::{Error, Result};
+use sqlx::SqlitePool;
+
+use crate::{fetch::Fetchable, models::anime::Anime, scrape::Scrapable};
 
 pub struct AnimeHandler<T: Fetchable + Scrapable> {
+    pub db_pool: SqlitePool,
     pub scraper: T,
 }
 
 impl<T: Fetchable + Scrapable> AnimeHandler<T> {
-    pub async fn search_animes(&self) {}
+    pub fn new(db_pool: SqlitePool, scraper: T) -> Self {
+        AnimeHandler { db_pool, scraper }
+    }
 
-    pub async fn get_anime(&self) {}
+    pub async fn search_animes(&self, query: &str) -> Vec<Anime> {
+        Vec::new()
+    }
 
-    pub async fn get_media(&self) {}
+    pub async fn get_anime(&self, anime_id: &str) -> Result<Anime> {
+        Err(Error::msg(""))
+    }
+
+    pub async fn get_media(&self, anime_id: &str, media_id: &str) -> Result<Anime> {
+        Err(Error::msg(""))
+    }
 }

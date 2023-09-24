6dd4f848249a08ed0f7f684be85b412f850dba31
LordShadd <real.davidwilson@proton.me>
2023-09-24 14:04:55 -0300

    refactor: use phantom data

--- src/anime_handler.rs
+++ src/anime_handler.rs
@@ -1,20 +1,40 @@
 use anyhow::{Error, Result};
 use sqlx::SqlitePool;
+use std::marker::PhantomData;
+use std::ops::Deref;
 
+use crate::models::anime::{AnimeType, PartialAnime};
+use crate::scrape::ScrapedSearch;
 use crate::{fetch::Fetchable, models::anime::Anime, scrape::Scrapable};
 
-pub struct AnimeHandler<T: Fetchable + Scrapable> {
+pub struct AnimeHandler<S: Scrapable + Fetchable> {
     pub db_pool: SqlitePool,
-    pub scraper: T,
+    _marker: PhantomData<*const S>,
 }
 
-impl<T: Fetchable + Scrapable> AnimeHandler<T> {
-    pub fn new(db_pool: SqlitePool, scraper: T) -> Self {
-        AnimeHandler { db_pool, scraper }
+impl<S: Scrapable + Fetchable> AnimeHandler<S> {
+    pub fn new(db_pool: SqlitePool) -> Self {
+        AnimeHandler {
+            db_pool,
+            _marker: PhantomData,
+        }
     }
 
-    pub async fn search_animes(&self, query: &str) -> Vec<Anime> {
-        Vec::new()
+    pub async fn search_anime(&self, query: &str) -> Result<Vec<PartialAnime>> {
+        let data = S::fetch_search(query, 1).await?;
+        let result = S::scrape_search(data).await?;
+
+        let animes = result
+            .animes
+            .into_iter()
+            .map(|a| PartialAnime {
+                title: Some(a.title),
+                poster_url: a.poster_url,
+                ..Default::default()
+            })
+            .collect();
+
+        Ok(animes)
     }
 
     pub async fn get_anime(&self, anime_id: &str) -> Result<Anime> {
